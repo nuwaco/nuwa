1a9049051897194676e40d2356589167953fb97a
wangjunjun <wangjunjun@xhsd.com>
2020-05-01 05:13:25 +0800

    feat(frank):modify brand name

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -103,6 +103,14 @@ module AccountsHelper
   end
 
   def svg_logo_full
+    #content_tag(:span, class: 'display-name')
+    #content_tag(:svg, tag(:use, 'xlink:href' => '#mastodon-svg-logo-full_2'), 'viewBox' => '0 0 713.35878 175.8678')
     content_tag(:svg, tag(:use, 'xlink:href' => '#mastodon-svg-logo-full'), 'viewBox' => '0 0 713.35878 175.8678')
+  end
+
+  def svg_logo_full_2
+    #content_tag(:span, class: 'display-name')
+    content_tag(:svg, tag(:use, 'xlink:href' => '#mastodon-svg-logo-full_2'), 'viewBox' => '0 0 713.35878 175.8678')
+    #content_tag(:svg, tag(:use, 'xlink:href' => '#mastodon-svg-logo-full'), 'viewBox' => '0 0 713.35878 175.8678')
   end
 end
