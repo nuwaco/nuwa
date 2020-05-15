dd1a8923a80f4322958585f741f3a3ea927b508d
wangjunjun <wangjunjun@xhsd.com>
2020-05-16 06:54:23 +0800

    feat(wangjunjun):modify start.sh

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+random=$RANDOM
+echo `date` "--" $random "--deployed begin!!"
 git pull
 docker stop $(sudo docker ps -a -q) 
 docker  rm $(sudo docker ps -a -q)
-nohup docker-compose build && docker-compose up -d &
+docker-compose build 
+docker-compose up -d 
+systemctl stop nginx
+systemctl start nginx
+echo `date` "--" $random "--deployed done!!"
